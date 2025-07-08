e9b48d7b15f7e4fa5c3f172bfe9352773a006368
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-08 19:57:35 +0300

    Delete hospital-management-system directory

--- hospital-management-system/app/utils/date.js
+++
@@ -1,9 +0,0 @@
-import { format } from 'date-fns';
-
-export function formatDate(date, pattern = 'PPp') {
-  try {
-    return date && !isNaN(new Date(date)) ? format(new Date(date), pattern) : 'N/A';
-  } catch {
-    return 'N/A';
-  }
-}
