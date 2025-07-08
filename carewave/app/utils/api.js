67155d0a76deebfcd77637e7e0837f501298c7dd
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-08 21:49:58 +0300

    Update api.js

--- carewave/app/utils/api.js
+++ carewave/app/utils/api.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
 export function useApiData(fetchFunction, dependencies = []) {
