dfdf394c91a8a1ad7bfa2c473e49865c0e7857d4
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-11 19:53:08 +0300

    Update LoginForm.jsx

--- carewave/app/auth/LoginForm.jsx
+++ carewave/app/auth/LoginForm.jsx
@@ -156,7 +156,7 @@ export default function LoginForm() {
 
             <div className="text-center">
               <p className="text-gray-600 mb-4">
-                Don't have an account?{' '}
+                Don&apos;t have an account?{' '}
                 <Link
                   href="/auth"
                   className="text-blue-600 hover:text-blue-700 font-semibold transition-colors"
