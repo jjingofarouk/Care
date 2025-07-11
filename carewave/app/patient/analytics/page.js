020a7bf381ff30203d8351537a5cfe8f69cc8b2c
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-11 22:44:53 +0300

    Create page.js

---
+++ carewave/app/patient/analytics/page.js
@@ -0,0 +1,6 @@
+// app/patients/analytics/page.js
+import PatientAnalytics from '../../components/patients/PatientAnalytics';
+
+export default function PatientAnalyticsPage() {
+  return <PatientAnalytics />;
+}
