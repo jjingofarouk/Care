87bb12a37396cdf5bef26bd18229446d109eab40
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-11 23:18:08 +0300

    Update page.js

--- carewave/app/patient/edit/[id]/page.js
+++ carewave/app/patient/edit/[id]/page.js
@@ -1,4 +1,5 @@
-// app/patients/edit/[id]/page.js
+// app/patients/edit/[id]/page.
+import React from 'react';
 import PatientForm from '../../../components/patients/PatientForm';
 
 export default function EditPatientPage() {
