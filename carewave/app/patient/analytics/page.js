0e63f835567490cebd2ac775663bcc17e3d75ecc
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-11 23:17:44 +0300

    Update page.js

--- carewave/app/patient/analytics/page.js
+++ carewave/app/patient/analytics/page.js
@@ -1,4 +1,8 @@
+
+
 // app/patients/analytics/page.js
+
+import React from 'react';
 import PatientAnalytics from '../../components/patients/PatientAnalytics';
 
 export default function PatientAnalyticsPage() {
