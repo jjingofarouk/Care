968db1b2a9eec28da31d4502122a468ad68870cf
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-12 17:00:46 +0300

    Update page.js

--- carewave/app/doctors/specializations/page.js
+++ carewave/app/doctors/specializations/page.js
@@ -1,5 +1,5 @@
 'use client';
-
+import React from 'react';
 import { useState, useEffect } from 'react';
 import SpecializationTable from '../../components/doctors/SpecializationTable';
 import SpecializationForm from '../../components/doctors/SpecializationForm';
