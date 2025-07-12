e428d2897f08156e05a2d1c2085ab0a05ebf3559
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-12 21:29:38 +0300

    Update page.jsx

--- carewave/app/appointments/visit-types/page.jsx
+++ carewave/app/appointments/visit-types/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { Typography, Button, Table, TableBody, TableCell, TableHead, TableRow, IconButton } from '@mui/material';
 import { PlusCircle, Edit, Trash2 } from 'lucide-react';
