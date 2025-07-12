b8e43c2f984d531ba502a6a9ac7c9ddc283c430d
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-12 17:01:04 +0300

    Update page.js

--- carewave/app/departments/page.js
+++ carewave/app/departments/page.js
@@ -1,5 +1,5 @@
 'use client';
-
+import React from 'react';
 import { useState, useEffect } from 'react';
 import DepartmentTable from '../components/departments/DepartmentTable';
 import DepartmentForm from '../components/departments/DepartmentForm';
