db606f2c67aac18b07917ec491f7f588e14cfe52
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-13 19:39:02 +0300

    Update page.js

--- carewave/app/medical-records/analytics/page.js
+++ carewave/app/medical-records/analytics/page.js
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 import MedicalRecordAnalytics from '../../components/medical-records/MedicalRecordAnalytics';
 
 export default function MedicalRecordAnalyticsPage() {
