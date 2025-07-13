2403a4cfa02ba7d9b097ede58512123c0ef67888
flowershadrahs <flowershadrah@gmail.com>
2025-07-13 05:57:17 +0300

    Update page.js

--- carewave/app/medical-records/page.js
+++ carewave/app/medical-records/page.js
@@ -1,10 +1,10 @@
 'use client';
 import React from 'react';
 import { useState, useEffect } from 'react';
-import medicalRecordsService from '../../services/medicalRecordsService';
-import MedicalRecordList from '../../components/medical-records/MedicalRecordList';
-import MedicalRecordFilter from '../../components/medical-records/MedicalRecordFilter';
-import MedicalRecordStats from '../../components/medical-records/MedicalRecordStats';
+import medicalRecordsService from '@/services/medicalRecordsService';
+import MedicalRecordList from '@/components/medical-records/MedicalRecordList';
+import MedicalRecordFilter from '@/components/medical-records/MedicalRecordFilter';
+import MedicalRecordStats from '@/components/medical-records/MedicalRecordStats';
 
 export default function MedicalRecordsPage() {
   const [records, setRecords] = useState([]);
