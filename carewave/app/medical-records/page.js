0882ee9627117f96773194b910f49790da300bd1
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-13 18:16:11 +0300

    Update page.js

--- carewave/app/medical-records/page.js
+++ carewave/app/medical-records/page.js
@@ -2,8 +2,6 @@
 import React from 'react';
 import MedicalRecordsView from '@/components/medical-records/MedicalRecordsView';
 
-// Prevent static generation
-export const dynamic = 'force-dynamic';
 
 export default function MedicalRecordsPage() {
   return <MedicalRecordsView />;
