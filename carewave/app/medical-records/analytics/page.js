3c117ad957546bfcb085bb1e658aa2fab6d6a519
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-13 19:37:00 +0300

    Create page.js

---
+++ carewave/app/medical-records/analytics/page.js
@@ -0,0 +1,6 @@
+'use client';
+import MedicalRecordAnalytics from '../../components/medical-records/MedicalRecordAnalytics';
+
+export default function MedicalRecordAnalyticsPage() {
+  return <MedicalRecordAnalytics />;
+}
