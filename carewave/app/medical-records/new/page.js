9c09c9ffa7a77c802a6f4150fb1d0c731efa1e9f
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-13 19:36:40 +0300

    Create page.js

---
+++ carewave/app/medical-records/new/page.js
@@ -0,0 +1,6 @@
+'use client';
+import MedicalRecordForm from '../../components/medical-records/MedicalRecordForm';
+
+export default function NewMedicalRecordPage() {
+  return <MedicalRecordForm />;
+}
