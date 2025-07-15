05146ba71a8a36ae75a773dac637aefded0d8c32
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-15 06:00:33 +0300

    Update useAuth.js

--- carewave/app/useAuth.js
+++ carewave/app/useAuth.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import { jwtDecode } from 'jwt-decode';
 import { login, logout, refreshToken } from './auth/authService';
