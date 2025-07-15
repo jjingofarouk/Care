ace19576f7854ff1af7ad0c9b2314622bb9c2f6e
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-15 21:11:00 +0300

    Update page.js

--- carewave/app/emergency/[id]/page.js
+++ carewave/app/emergency/[id]/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { emergencyService } from '../../services/emergencyService';
 
