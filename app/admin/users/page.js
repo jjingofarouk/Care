691c009423eca67a84483171480406a3f568fe59
jjingofarouk <jjingofaroukk@gmail.com>
2025-09-21 00:37:33 +0300

    still fixing auth

--- app/admin/users/page.js
+++ app/admin/users/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { UserPlus, Trash2, Edit } from 'lucide-react';
 import { useAuth } from '@/auth/AuthContext';
 
